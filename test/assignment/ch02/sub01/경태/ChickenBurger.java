c014f198e847665054d825760a20f68583fd7397
gt.park <smileboy0014@gmail.com>
2023-09-26 20:51:29 +0900

    [item2] - item2 문제에 대한 풀이(Item2AnswerTest)

---
+++ test/assignment/ch02/sub01/경태/ChickenBurger.java
@@ -0,0 +1,37 @@
+package assignment.ch02.sub01.경태;
+
+public class ChickenBurger extends Hamburger {
+
+    private final boolean extraSauce; // 선택 매개변수
+
+
+    ChickenBurger(Builder builder) {
+        super(builder);
+        extraSauce = builder.extraSauce;
+    }
+
+    public boolean isExtraSauce() {
+        return extraSauce;
+    }
+
+    public static class Builder extends Hamburger.Builder<Builder>{
+
+        private boolean extraSauce;
+
+        public Builder addExtraSauce() {
+            extraSauce = true;
+            return this;
+        }
+
+        @Override
+        ChickenBurger build() {
+            return new ChickenBurger(this);
+        }
+
+        @Override
+        protected Builder self() {
+            return this;
+        }
+
+    }
+}
