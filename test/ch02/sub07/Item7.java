a479bee491dca1a95c63ca0fef59045b678bbe9e
suyeah <sy.han2@okestro.com>
2023-10-16 19:43:12 +0900

    문제 7

--- test/ch02/sub07/Item7.java
+++ test/ch02/sub07/Item7.java
@@ -4,12 +4,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
+import java.util.WeakHashMap;
 
 class Item7 {
     @DisplayName("아래는 HashMap으로 작성된 코드 이다. WeakHashMap 으로 변경 후 key2, key3을 제거 후 출력하라.")
     @Test
     void item7_answer() {
-        final HashMap<Integer, String> hashMap = new HashMap<>();
+        // 1. WeakHashMap 사용 코드로 수정
+        final WeakHashMap<Integer, String> hashMap = new WeakHashMap<>();
 
         Integer key1 = 1000;
         Integer key2 = 2000;
@@ -19,6 +21,12 @@ class Item7 {
         hashMap.put(key2, "test2");
         hashMap.put(key3, "test3");
 
+        key2 = null;
+        key3 = null;
+
+        System.gc();
+
         hashMap.entrySet().forEach(System.out::println);
+        // 2. 다 쓴 엔트리가 즉시 자동으로 제거된다
     }
 }
