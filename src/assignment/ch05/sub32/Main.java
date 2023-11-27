d70f720a9683b45178353cce9b76142f52baf724
hwLee <hw.lee@okestro.com>
2023-11-27 13:56:38 +0900

    hw - 문제 및 답안 업로드

--- src/assignment/ch05/sub32/Main.java
+++ src/assignment/ch05/sub32/Main.java
@@ -1,7 +1,22 @@
 package assignment.ch05.sub32;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Main {
+    static <T> List<T> pickTwo(T a, T b, T c) {
+        switch (ThreadLocalRandom.current().nextInt(3)) { // 랜덤 0~2
+            case 0: return Arrays.asList(a, b); // warning:Unchecked generics array creation for varargs parameter
+            case 1: return Arrays.asList(a, c); // warning:Unchecked generics array creation for varargs parameter
+            case 2: return Arrays.asList(b, c); // warning:Unchecked generics array creation for varargs parameter
+        }
+        throw new AssertionError();
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<String> attrs = pickTwo("좋은", "빠른", "저렴한");
+        System.out.println("attrs = " + attrs.toString());
     }
 }
+
--- src/assignment/ch06/sub35/Main.java
+++ src/assignment/ch06/sub35/Main.java
@@ -1,7 +1,36 @@
 package assignment.ch06.sub35;
 
 public class Main {
+    public static final int RESULT_CD_200 = 200;
+    public static final String RESULT_CD_TEXT_200 = "OK";
+
+    public static final int RESULT_CD_201 = 201;
+    public static final String RESULT_CD_TEXT_201 = "Created";
+
+    public static final int RESULT_CD_202 = 202;
+    public static final String RESULT_CD_TEXT_202 = "Accepted";
+
+    public static final int RESULT_CD_400 = 400;
+    public static final String RESULT_CD_TEXT_400 = "Bad Request";
+
+    public static final int RESULT_CD_401 = 401;
+    public static final String RESULT_CD_TEXT_401 = "Unauthorized";
+
+    public static final int RESULT_CD_404 = 404;
+    public static final String RESULT_CD_TEXT_404 = "Not Found";
+
+    public static final int RESULT_CD_409 = 409;
+    public static final String RESULT_CD_TEXT_409 = "Conflict";
+
+    public static final int RESULT_CD_422 = 422;
+    public static final String RESULT_CD_TEXT_422 = "Unprocessable Entity";
+
+    public static final int RESULT_CD_500 = 500;
+    public static final String RESULT_CD_TEXT_500 = "Internal Server Error or Unknown Error";
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Result Code : " + RESULT_CD_200);
+        System.out.println("Result Text : " + RESULT_CD_TEXT_200);
     }
 }
