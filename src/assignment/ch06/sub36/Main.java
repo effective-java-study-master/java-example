0cec3838f161b9f1747499b665c2f9917c467805
hwLee <hw.lee@okestro.com>
2023-11-27 13:56:38 +0900

    hw - 문제 및 답안 업로드

--- src/assignment/ch06/sub36/Main.java
+++ src/assignment/ch06/sub36/Main.java
@@ -1,7 +1,22 @@
 package assignment.ch06.sub36;
 
+import java.util.EnumSet;
+import java.util.Objects;
+import java.util.Set;
+
+
+class Text {
+    public enum Style {BOLD, ITALIC, UNDERLINE, STRIKETHROUGH}
+
+    public void applyStyles(Set<Style> styles) {
+        System.out.printf("Applying styles %s to text%n",
+                Objects.requireNonNull(styles));
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Text text = new Text();
+        text.applyStyles(EnumSet.of(Text.Style.BOLD, Text.Style.ITALIC));
     }
 }
--- src/assignment/ch06/sub37/Main.java
+++ src/assignment/ch06/sub37/Main.java
@@ -1,7 +1,43 @@
 package assignment.ch06.sub37;
 
+import java.util.*;
+
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toSet;
+
+class Plant {
+    enum LifeCycle { ANNUAL, PERENNIAL, BIENNIAL }  // 생애주기
+
+    final String name;
+    final LifeCycle lifeCycle;
+
+    Plant(String name, LifeCycle lifeCycle) {
+        this.name = name;
+        this.lifeCycle = lifeCycle;
+    }
+
+    @Override public String toString() {
+        return name;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Plant[] garden = new Plant[3];
+        garden[0] = new Plant("test1", Plant.LifeCycle.ANNUAL);
+        garden[1] = new Plant("test2", Plant.LifeCycle.PERENNIAL);
+        garden[2] = new Plant("test3", Plant.LifeCycle.BIENNIAL);
+
+        Map<Plant.LifeCycle, Set<Plant>> plantsByLifeCycle = new EnumMap<>(Plant.LifeCycle.class);  // EnumMap
+
+        for (Plant.LifeCycle lc : Plant.LifeCycle.values())
+            plantsByLifeCycle.put(lc, new HashSet<>());
+
+        for (Plant p : garden)
+            plantsByLifeCycle.get(p.lifeCycle).add(p);
+
+        System.out.println(plantsByLifeCycle);
+
+        System.out.println(Arrays.stream(garden).collect(groupingBy(p -> p.lifeCycle, () -> new EnumMap<>(Plant.LifeCycle.class), toSet())));
     }
 }
