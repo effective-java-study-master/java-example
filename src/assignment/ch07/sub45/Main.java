6d8a48f1f70f2fe107495d814eee65c9d616a298
hwLee <hw.lee@okestro.com>
2023-12-05 11:14:38 +0900

    hw - 9주차 문제 완료

--- src/assignment/ch07/sub45/Main.java
+++ src/assignment/ch07/sub45/Main.java
@@ -1,7 +1,22 @@
 package assignment.ch07.sub45;
 
-public class Main {
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
+
+class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<String> fruitNames = Arrays.asList("apple", "orange", "banana", "grape", "apricot", "kiwi");
+
+        // 스트림 파이프라인
+        List<String> result = fruitNames.stream().filter(e -> e.charAt(0) == 'a').collect(toList());
+        // 중간연산: "a"로 시작하는 원소 찾기
+        // 종단연산: 리스트로 모으기
+
+        // 결과 출력
+        result.forEach(System.out::println);
     }
 }
